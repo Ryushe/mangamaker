fbb637361c4e1622c3f653b014f8f3d453249fdb
Ryushe <ryushe.dev@gmail.com>
2024-07-30 14:05:28 -0700

    working site class

--- utils/download_katana_zips.py
+++ utils/download_katana_zips.py
@@ -2,9 +2,13 @@
 from utils.web_search import Site
 from selenium.webdriver.common.by import By
 
-def main(search_query):
-    manga_katana = Site("https://mangakatana.com/")
-    manga_katana.set_search_elements("input_search", "searchsubmit", "a", "book_list", By.ID)
+
+def main(search_query, output_dir):
+    manga_katana = Site("https://mangakatana.com/", "test")
+    manga_katana.set_search_elements("input_search",
+                                      "searchsubmit", 
+                                      "title", By.CLASS_NAME,
+                                      "a", By.TAG_NAME)
     url = manga_katana.search_for_manga_url(search_query)
     print(url)
 
